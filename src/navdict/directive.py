56789789c3994b9c30dc7478cd030abde0fc5fa6
Rik Huygen <rik.huygen@kuleuven.be>
2025-08-12 11:59:04 +0200

    remove a left-over print statement

--- src/navdict/directive.py
+++ src/navdict/directive.py
@@ -7,6 +7,7 @@ __all__ = [
     "register_directive",
 ]
 
+import logging
 import re
 from importlib.metadata import EntryPoint
 from importlib.metadata import entry_points
@@ -15,6 +16,7 @@ from typing import overload
 
 DIRECTIVE_PATTERN = re.compile(r"^([a-zA-Z]\w+)/{2}(.*)$")
 
+logger = logging.getLogger("navdict")
 
 class Directive:
     @overload
@@ -60,7 +62,7 @@ def load_directive_plugins():
     global _directive_plugins
 
     eps = entry_points()
-    print(sorted(eps.groups))
+    # logger.debug(f"entrypoint groups: {sorted(eps.groups)}")
     eps = eps.select(group="navdict.directive")
 
     for ep in eps:
